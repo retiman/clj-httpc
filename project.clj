72e5ef59d58de20c87474e20a83b43d96e27ed23
Min Huang <min.huang@alumni.usc.edu>
2010-12-16 14:06:03 -0800

    Upgraded to httpclient 4.0.3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.clojure/clojure-contrib "1.2.0"]
-     [org.apache.httpcomponents/httpclient "4.0.1"]
+     [org.apache.httpcomponents/httpclient "4.0.3"]
      [commons-codec "1.4"]
      [commons-io "1.4"]
      [com.google.gdata/core "1.0"]]
