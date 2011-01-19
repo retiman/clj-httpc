44c3152bc6f78d526d53de84c4053ef6e459030f
Nate Murray <nate@natemurray.com>
2011-01-19 12:50:33 -0800

    added TrustEveryoneSSLSocketFactory

--- src/clj_httpc/TrustEveryoneSSLSocketFactory.java
+++ src/clj_httpc/TrustEveryoneSSLSocketFactory.java
@@ -36,17 +36,19 @@ public class TrustEveryoneSSLSocketFactory {
         try {
             SSLContext sslContext = SSLContext.getInstance("SSL");
             // set up a TrustManager that trusts everything
-            sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
+            sslContext.init(null, 
+                            new TrustManager[] { 
+                                new X509TrustManager() {
+                                    public X509Certificate[] getAcceptedIssuers() {
+                                        return null;
+                                    }
 
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                    }
+                                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                                    }
 
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                    }
-                } }, new SecureRandom());
+                                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                                    }
+                                } }, new SecureRandom());
 
             SSLSocketFactory sf = new SSLSocketFactory(sslContext);
             return sf;
