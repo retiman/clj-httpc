3e4f28b0878e3852cd567e9db376fe4626095666
Min Huang <mhuang@attinteractive.com>
2011-01-19 17:17:34 -0800

    Formatting

--- src/clj_httpc/TrustEveryoneSSLSocketFactory.java
+++ src/clj_httpc/TrustEveryoneSSLSocketFactory.java
@@ -32,30 +32,30 @@ import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
 
 public class TrustEveryoneSSLSocketFactory {
-    public static SSLSocketFactory getSocketFactory() {
-        try {
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            // set up a TrustManager that trusts everything
-            sslContext.init(null,
-                            new TrustManager[] {
-                                new X509TrustManager() {
-                                    public X509Certificate[] getAcceptedIssuers() {
-                                        return null;
-                                    }
+  public static SSLSocketFactory getSocketFactory() {
+    try {
+      SSLContext sslContext = SSLContext.getInstance("SSL");
+      // set up a TrustManager that trusts everything
+      sslContext.init(null,
+          new TrustManager[] {
+            new X509TrustManager() {
+              public X509Certificate[] getAcceptedIssuers() {
+                return null;
+              }
 
-                                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                                    }
+              public void checkClientTrusted(X509Certificate[] certs, String authType) {
+              }
 
-                                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                                    }
-                                } }, new SecureRandom());
+              public void checkServerTrusted(X509Certificate[] certs, String authType) {
+              }
+            } }, new SecureRandom());
 
-            SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-            return sf;
-        } catch (java.security.KeyManagementException e) {
-            return null;
-        } catch (java.security.NoSuchAlgorithmException e) {
-            return null;
-        }
+      SSLSocketFactory sf = new SSLSocketFactory(sslContext);
+      return sf;
+    } catch (java.security.KeyManagementException e) {
+      return null;
+    } catch (java.security.NoSuchAlgorithmException e) {
+      return null;
     }
+  }
 }
