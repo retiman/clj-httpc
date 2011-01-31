cbf6f6ff38f21ce81bd2165907854b774676846c
Min Huang <mhuang@attinteractive.com>
2011-01-31 15:06:42 -0800

    Upgrade to httpclient 4.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.clojure/clojure-contrib "1.2.0"]
-     [org.apache.httpcomponents/httpclient "4.1-beta1"]
+     [org.apache.httpcomponents/httpclient "4.1"]
      [log4j "1.2.15" :exclusions
        [javax.mail/mail
         javax.jms/jms
