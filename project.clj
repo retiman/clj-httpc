540f744254a0bab06575f56b6b2c57fb605314d5
Min Huang <mhuang@attinteractive.com>
2011-01-31 12:57:34 -0800

    Clean up some of the deps

--- project.clj
+++ project.clj
@@ -7,14 +7,14 @@
     [[org.clojure/clojure "1.2.0"]
      [org.clojure/clojure-contrib "1.2.0"]
      [org.apache.httpcomponents/httpclient "4.1-beta1"]
-     [commons-logging "1.0.4"]
-     [log4j/log4j "1.2.15" :exclusions
+     [log4j "1.2.15" :exclusions
        [javax.mail/mail
         javax.jms/jms
         com.sun.jdmk/jmxtools
         com.sun.jmx/jmxri]]
      [commons-codec "1.4"]
      [commons-io "1.4"]
+     [commons-logging "1.0.4"]
      [com.google.gdata/core "1.0"]]
   :dev-dependencies
     [[lein-javac "1.2.1-SNAPSHOT"]
